d2d623d06237068d094272241f343d4fd3e09de2
JesseBadBoy <kyletice@gmail.com>
2025-02-23 20:17:42 -0500

    added pygame...tick

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.time.Clock()
+    dt = 0
 
 #creating an infinite game loop
     while True:
@@ -16,6 +18,9 @@ def main():
                 return
         pygame.Surface.fill(screen,(0,0,0))
         pygame.display.flip
+        clock = pygame.time.Clock()
+        rt = clock.tick(60)
+        dt = rt/1000
 
 
 if __name__ == "__main__":
