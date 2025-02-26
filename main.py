62fa89249e8296397487274dc891be9167574423
JesseBadBoy <kyletice@gmail.com>
2025-02-25 23:19:43 -0500

    drawing player at the center of the screen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
-#import constants
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -10,17 +10,23 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
+#creating sprites
+
 
 #creating an infinite game loop
+    clock = pygame.time.Clock()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0))
-        pygame.display.flip
-        clock = pygame.time.Clock()
+        screen.fill((0,0,0))
+        player.draw(screen)
+        pygame.display.flip()
         rt = clock.tick(60)
         dt = rt/1000
+
 
 
 if __name__ == "__main__":
