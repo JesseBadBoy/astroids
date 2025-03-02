3327d5ff259e6a0f65b23f6d101aac7e6796dbeb
JesseBadBoy <kyletice@gmail.com>
2025-03-01 22:41:44 -0500

    asteroidfield is now porducing asteroids

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def main():
     asteroids = pygame.sprite.Group()
     Asteroid.containers = (asteroids, updatable, drawable)
 
-    asteriod = AsteroidField
     AsteroidField.containers = (updatable)
+    asteriod_field = AsteroidField()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.time.Clock()
